e0b9d941ec86af624eeb2a03b8382f87eab0b701
Mqc <87596784+MqcCheeze@users.noreply.github.com>
2023-12-13 20:04:59 +0000

    Added more sh*tpost

--- src/index.js
+++ src/index.js
@@ -20,7 +20,26 @@ client.on("ready", (c) => { // When bot initialises
     console.log(`${c.user.username} is online...`)
 });
 
-const replies = ["stfu lol", "ratio", "cry about it", "beta", "L"];
+const greetings = [ "YAHOOOO", "hey!!", "haiii", "how is everyone!!! :3", "hellooo", "hi freiens" ]
+
+const Greeting = () => {
+    try {
+        const channel = client.channels.cache.get("1180115044218978425").send(getRandomArrElement(greetings));
+    } catch {
+        return;
+    }
+};
+
+setInterval(Greeting, 60000);
+
+const ratioReplies = [ "stfu lol", "ratio", "cry about it", "beta", "L" ];
+const confusedReplies = [ "what do u not get lol not that hard to understand...", 
+                            "are u stoopid?", 
+                            "lol dumb hehe", 
+                            "silly you", 
+                            ":nerd:" ];
+const noReplies = [ "yes", "actually... yes", "yeah lol", "who asked", "nah bro" ];
+const yesReplies = [ "no", "nah bro yes*", "lmao... NO!", " actually... no...", "uhm no?" ];
 
 client.on("messageCreate", (message) => { // When bot reads message
 
@@ -28,7 +47,7 @@ client.on("messageCreate", (message) => { // When bot reads message
         const slapMsg = message.content.split(" ");
         console.log(slapMsg);
         for (const word of slapMsg) {
-            if (word === "slaps" || word === "ties" || word === "bonks") {
+            if (word === "slaps" || word === "ties" || word === "bonks" || word === "punches") {
                 message.reply(`${message.mentions.users.first()} OWW why did you do that :sob:`);
                 break;
             }
@@ -40,8 +59,14 @@ client.on("messageCreate", (message) => { // When bot reads message
         return
     }
 
-    if(message.content === "wtf" || message.content === "Wtf" || message.content === "wth" || message.content === "Wth") {
-        message.reply(getRandomArrElement(replies));
+    if (message.content === "wtf" || message.content === "Wtf" || message.content === "wth" || message.content === "Wth") {
+        message.reply(getRandomArrElement(ratioReplies));
+    } else if (message.content === "huh" || message.content === "hoh" || message.content === "Huh" || message.content === "Hoh" || message.content === "what" || message.content === "What") {
+        message.reply(getRandomArrElement(confusedReplies));
+    } else if (message.content === "yes" || message.content === "Yes" || message.content === "yeah" || message.content === "Yeah") {
+        message.reply(getRandomArrElement(noReplies));
+    }  else if (message.content === "no" || message.content === "No" || message.content === "nah" || message.content === "Nah") {
+        message.reply(getRandomArrElement(yesReplies));
     }
 
     if (message.content === "pong"){
@@ -81,7 +106,7 @@ client.on("interactionCreate", (interaction) => {
 
 function getRandomArrElement(array) {
     const replyIndex = Math.floor(Math.random() * array.length);
-    const selectedReply = replies[replyIndex];
+    const selectedReply = array[replyIndex];
     return selectedReply;
 }
 
